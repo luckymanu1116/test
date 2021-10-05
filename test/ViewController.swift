1426c78225741ace09f320b8617c227b5cc675bd
Manoj Reddy <guttikondamanoj@gmail.com>
2021-10-05 18:31:56 +0530

    initial

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var reg: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        reg.layer.cornerRadius = 10
+        
+
     }
+    
 
 
 }
